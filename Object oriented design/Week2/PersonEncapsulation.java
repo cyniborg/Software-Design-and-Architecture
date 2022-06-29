4ae2d0acc17962aced7ed266c00cc70f6a220b5b
Robin John <cyniborg@gmail.com>
2022-06-29 12:23:17 +0200

    Adding week two Person encapsulation java class

---
+++ Object oriented design/Week2/PersonEncapsulation.java
@@ -0,0 +1,70 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender ;
+    private String countryOfResidence;
+    private String nationality;
+    private String profession;
+
+    public Person(String firstName, String lastName, int age, String gender, String countryOfResidence, String nationality, String profession) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.countryOfResidence = countryOfResidence;
+        this.nationality = nationality;
+        this.profession = profession;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName (String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastName;
+    }
+    public void setLastName (String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountryOfResidence() {
+        return countryOfResidence;
+    }
+    public void setCountryOfResidence (String countryOfResidence) {
+        this.countryOfResidence = countryOfResidence;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge (int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+    public void setGender (String gender) {
+        this.gender = gender;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+    public void setNationality (String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+    public void setProfession (String profession) {
+        this.profession = profession;
+    }
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+}
